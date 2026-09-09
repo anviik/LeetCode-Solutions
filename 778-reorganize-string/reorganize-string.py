cf73e1356a888473ea8e40c01f53d810c44d9310
Anvi <107345013+anviik@users.noreply.github.com>
2026-09-08 21:16:00 -0700

    Time: 0 ms (100.00%) | Memory: 19.5 MB (21.41%) - LeetSync

--- 778-reorganize-string/reorganize-string.py
+++ 778-reorganize-string/reorganize-string.py
@@ -1,23 +1,19 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
         count = Counter(s)
-        maxheap = [[-v, k] for k, v in count.items()]
+        maxheap = [[-v, k] for k,v in count.items()]
+        res = []
         prev = None
         heapq.heapify(maxheap)
-        news = []
         while maxheap or prev:
             if prev and not maxheap:
                 return ""
             cnt, char = heapq.heappop(maxheap)
-            news.append(char)
+            res.append(char)
             cnt += 1
             if prev:
-                heapq.heappush(maxheap,prev)
+                heapq.heappush(maxheap, prev)
                 prev = None
             if cnt != 0:
                 prev = [cnt, char]
-        return "".join(news)
-
-
-
-            #put in counter, if theres muttiple of the same letter, put them at the end one at a time of the arr, if any letter occurs > half the time its not pssible
+        return "".join(res)
