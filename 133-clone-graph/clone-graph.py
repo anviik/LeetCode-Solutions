e03dd13482e5f3d616d1e3f5cd3fb2ee908fbb95
Anvi <107345013+anviik@users.noreply.github.com>
2026-09-20 01:08:49 -0700

    Time: 42 ms (91.83%) | Memory: 19.6 MB (98.09%) - LeetSync

---
+++ 133-clone-graph/clone-graph.py
@@ -0,0 +1,25 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return 
+        copy = {}
+        def dfs(node):
+            if node in copy:
+                return copy[node]
+            newnode = Node(node.val)
+            copy[node] = newnode
+            for n in node.neighbors:
+                newnode.neighbors.append(dfs(n))
+            return newnode
+        return dfs(node)
+
+        
